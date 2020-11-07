2b50952233a78802fc92b834f8114faf45225877
Shawn Krisman <shawn@patreon.com>
2020-11-07 01:06:16 -0800

    create HarvesterCreep class.

--- src/roles/harvester.ts
+++ src/roles/harvester.ts
@@ -1,11 +1,19 @@
 import includes from 'lodash/includes'
 
 import { fromBodyPlan, byPartCount, planCost } from 'utils/parts'
-import { wrap } from 'utils/profiling'
+import { profile } from 'utils/profiling'
 import * as Logger from 'utils/logger'
 import { spawnCreep } from 'utils/spawn'
 
 const ROLE = 'harvester'
+
+const BODY_PLANS = [
+    byPartCount({ [MOVE]: 10, [WORK]: 10, [CARRY]: 16 }),
+    byPartCount({ [MOVE]: 10, [WORK]: 10, [CARRY]: 8 }),
+    byPartCount({ [MOVE]: 10, [WORK]: 10, [CARRY]: 4 }),
+    byPartCount({ [MOVE]: 7, [WORK]: 7, [CARRY]: 2 }),
+    byPartCount({ [MOVE]: 5, [WORK]: 5 }),
+]
 
 export interface Harvester extends SourceCreep {
     memory: HarvesterMemory
@@ -15,38 +23,70 @@ interface HarvesterMemory extends SourceMemory {
     role: 'harvester'
 }
 
-const BODY_PLANS = [
-    byPartCount({ [MOVE]: 10, [WORK]: 10, [CARRY]: 6 }),
-    byPartCount({ [MOVE]: 5, [WORK]: 5, [CARRY]: 2 }),
-]
+export class HarvesterCreep {
+    readonly creep: Harvester
 
-const roleHarvester = {
-    run: wrap((creep: Harvester) => {
-        const roomMemory = Memory.rooms[creep.room.name]
-        const sourceMemory = roomMemory.sources.find(
-            s => s.id === creep.memory.source,
-        )
-        if (!sourceMemory) {
-            throw Error(`source memory isn't real ${roomMemory.sources}`)
+    constructor(creep: Harvester) {
+        this.creep = creep
+    }
+
+    @profile
+    run() {
+        if (this.creep.spawning) {
+            return
         }
+        const sourceMemory = this.getSourceMemory()
         const source = Game.getObjectById(sourceMemory.id) as Source
-        const harvestPos = sourceMemory.dropSpot.pos
-        if (creep.pos.x === harvestPos.x && creep.pos.y === harvestPos.y) {
-            const err = creep.harvest(source)
+        if (this.isAtHarvestPos()) {
+            const err = this.creep.harvest(source)
             if (!includes([OK, ERR_NOT_ENOUGH_RESOURCES], err)) {
                 Logger.warning(
                     'harvester:harvest:failure',
-                    creep.name,
+                    this.creep.name,
                     "couldn't harvest",
                     err,
                 )
             }
         } else {
-            creep.moveTo(harvestPos.x, harvestPos.y, {
-                visualizePathStyle: { stroke: '#ffaa00' },
-            })
+            this.moveToHarvestPos()
         }
-    }, 'runHarvester'),
+    }
+
+    getSourceMemory() {
+        const roomMemory = Memory.rooms[this.creep.room.name]
+        const sourceMemory = roomMemory.sources.find(
+            s => s.id === this.creep.memory.source,
+        )
+        if (!sourceMemory) {
+            throw Error(`source memory isn't real ${roomMemory.sources}`)
+        }
+        return sourceMemory
+    }
+
+    get harvestPos() {
+        const sourceMemory = this.getSourceMemory()
+        return sourceMemory.dropSpot.pos
+    }
+
+    isAtHarvestPos() {
+        return (
+            this.creep.pos.x === this.harvestPos.x &&
+            this.creep.pos.y === this.harvestPos.y
+        )
+    }
+
+    moveToHarvestPos() {
+        this.creep.moveTo(this.harvestPos.x, this.harvestPos.y, {
+            visualizePathStyle: { stroke: '#ffaa00' },
+        })
+    }
+}
+
+const roleHarvester = {
+    run: (creep: Harvester) => {
+        const harvester = new HarvesterCreep(creep)
+        harvester.run()
+    },
 
     create(spawn: StructureSpawn, source: Id<Source>, rescue = false): number {
         const capacity = rescue
